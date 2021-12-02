a61f4881f7adb79bfc93b4a4e07db2ec0687c785
rocka <rockimunoz94@gmail.com>
2021-12-02 09:27:54 -0300

    deploy

---
+++ precache-manifest.c8481d4ce8da383b666496a9707d29a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cd31c36fab8caa026ff28d840e6b53",
+    "url": "/chronometer_test/alarma.mp3"
+  },
+  {
+    "revision": "06bd82bf0cde9c92f639",
+    "url": "/chronometer_test/css/app.6cbe4401.css"
+  },
+  {
+    "revision": "0ad7788a80656a4188d25d62df2bc722",
+    "url": "/chronometer_test/img/icon.0ad7788a.png"
+  },
+  {
+    "revision": "345c0656ffec1b997cd9af1f4c1996d5",
+    "url": "/chronometer_test/index.html"
+  },
+  {
+    "revision": "f70a7e3fba04431d0105",
+    "url": "/chronometer_test/js/about.3e3c75ba.js"
+  },
+  {
+    "revision": "06bd82bf0cde9c92f639",
+    "url": "/chronometer_test/js/app.5bd6baf2.js"
+  },
+  {
+    "revision": "2c7364a3d45ab354880d",
+    "url": "/chronometer_test/js/chunk-vendors.b49cd335.js"
+  },
+  {
+    "revision": "4f06bfee532bf15d2a4abe2d681f9651",
+    "url": "/chronometer_test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chronometer_test/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/chronometer_test/precache-manifest.c8481d4ce8da383b666496a9707d29a6.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "timer_app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
